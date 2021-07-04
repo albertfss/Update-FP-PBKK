459d8a56df31a4dfb12af2faedbc736123cdc29a
Muhammad Farhan Haykal <70801807+neroSpace@users.noreply.github.com>
2021-07-04 19:28:38 +0700

    Update show.blade.php

--- ecommerce-laravel-8/resources/views/ecommerce/show.blade.php
+++ ecommerce-laravel-8/resources/views/ecommerce/show.blade.php
@@ -63,6 +63,9 @@
 					<li class="nav-item">
 						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Specification</a>
 					</li>
+                    <li class="nav-item">
+						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#review" role="tab" aria-controls="profile" aria-selected="false">Review</a>
+					</li>
 				</ul>
 				<div class="tab-content" id="myTabContent">
 					<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab" style="color: black">
